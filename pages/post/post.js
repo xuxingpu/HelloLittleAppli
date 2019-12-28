2c6b1f986473c040f0df445df729cb3579364971
xuxingpu <418149608@qq.com>
2019-12-29 02:07:10 +0800

    数据绑定

--- pages/post/post.js
+++ pages/post/post.js
@@ -4,15 +4,58 @@ Page({
   /**
    * 页面的初始数据
    */
-  data: {
-
-  },
+  data: {},
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log("onload:页面被加载")
+    var postList = [{
+      object: {
+        date: "Jan 29 2017"
+      },
+      title: "为自己奋斗",
+      postImg: "/images/post/book01.jpg",
+      avatar: "/images/post/fendou.jpg",
+      content: "在我们的人生历程中，我们希望自己变成一个什么样的人呢...不管我们希望变成一个怎样的人，对我们来说，只要我们真心实意地去做自己想做的事情，我们就会成功!",
+      readingNum: 108,
+      collectionNum: {
+        array: [92]
+      },
+      commentNum: 8
+    },
+      {
+        object: {
+          date: "Jan 28 2017"
+        },
+        title: "设计大师",
+        postImg: "/images/post/book02.jpg",
+        avatar: "/images/icon/icon-sheji00.png",
+        content: "设计大师...不仅仅是一个称号设计大师...不仅仅是一个称号",
+        readingNum: 1080,
+        collectionNum: {
+          array: [920]
+        },
+        commentNum: 833
+      },
+      {
+        object: {
+          date: "Jan 28 2017"
+        },
+        title: "孤本",
+        postImg: "/images/post/book03.jpg",
+        avatar: "/images/icon/icon-guben00.png",
+        content: "书籍作为文化、知识、思想的载体流传，不幸的是多湮灭于历史长河...我辈能得一孤本,幸甚至哉!",
+        readingNum: 108,
+        collectionNum: {
+          array: [92]
+        },
+        commentNum: 8
+      }]
+
+    this.setData({
+      postList: postList
+    })
   },
 
   /**
