a7691f157bc7f2de775448458c388f952cdfcd15
xuxingpu <418149608@qq.com>
2020-02-26 01:12:29 +0800

    增加评论区上传图片功能

--- db/DBPost.js
+++ db/DBPost.js
@@ -1,121 +1,132 @@
-var util = require('../utils/util.js');//引入util.js
+var util = require('../utils/util.js'); //引入util.js
 
 class DBPost {
 
-  constructor(postId) {
-    this.storageKeyName = 'postList';
-    this.postId = postId;
-  }
-
-  //得到全部文章信息
-  getAllPostData() {
-    var res = wx.getStorageSync(this.storageKeyName);
-    if (!res) {
-      res = require('../data/data.js').postList;
-      this.initPostList(res);
+    constructor(postId) {
+        this.storageKeyName = 'postList';
+        this.postId = postId;
     }
-    return res;
-  }
 
-  //保存或更新缓存数据
-  execStorageSync(data) {
-    wx.setStorageSync(this.storageKeyName, data);
-  }
-
-  //根据id获取文章数据
-  getPostItemById() {
-    var postData = this.getAllPostData();
-    var len = postData.length;
-    for (var i = 0; i < len; i++) {
-      if (postData[i].postId == this.postId) {
-        return {
-          //当前文章在缓存数据库中的序号
-          index: i,
-          data: postData[i]
+    //得到全部文章信息
+    getAllPostData() {
+        var res = wx.getStorageSync(this.storageKeyName);
+        if (!res) {
+            res = require('../data/data.js').postList;
+            this.initPostList(res);
         }
-      }
+        return res;
     }
-  }
 
-  //收藏
-  collect() {
-    return this.updatePostData('collect');
-  }
-  //点赞或者取消点赞
-  up() {
-    var data = this.updatePostData('up');
-    return data;
-  }
-  //更新本地的点赞、评论信息、收藏、阅读量
-  updatePostData(category) {//TODO 此处参数是否非category不可,是否可代替
-    var itemData = this.getPostItemById(),
-      postData = itemData.data,
-      allPostData = this.getAllPostData();
-    switch (category) {
-      case 'collect':
-        //收藏操作
-        if (!postData.collectionStatus) {
-          //如果当前状态是未收藏
-          postData.collectionNum++;
-          postData.collectionStatus = true;
+    //保存或更新缓存数据
+    execStorageSync(data) {
+        wx.setStorageSync(this.storageKeyName, data);
+    }
+
+    //根据id获取文章数据
+    getPostItemById() {
+        var postData = this.getAllPostData();
+        var len = postData.length;
+        for (var i = 0; i < len; i++) {
+            if (postData[i].postId == this.postId) {
+                return {
+                    //当前文章在缓存数据库中的序号
+                    index: i,
+                    data: postData[i]
+                }
+            }
+        }
+    }
+
+    //收藏
+    collect() {
+        return this.updatePostData('collect');
+    }
+    //点赞或者取消点赞
+    up() {
+        var data = this.updatePostData('up');
+        return data;
+    }
+    //更新本地的点赞、评论信息、收藏、阅读量
+    updatePostData(category, newComment) { //TODO 此处参数是否非category不可,是否可代替
+        var itemData = this.getPostItemById(),
+            postData = itemData.data,
+            allPostData = this.getAllPostData();
+        switch (category) {
+            case 'collect':
+                //收藏操作
+                if (!postData.collectionStatus) {
+                    //如果当前状态是未收藏
+                    postData.collectionNum++;
+                    postData.collectionStatus = true;
+                } else {
+                    //如果当前状态是已收藏
+                    postData.collectionNum--;
+                    postData.collectionStatus = false;
+                }
+                break;
+            case 'up':
+                //点赞操作
+                if (!postData.upStatus) {
+                    postData.upNum++;
+                    postData.upStatus = true;
+                } else {
+                    postData.upNum--;
+                    postData.upStatus = false;
+                }
+                break;
+            case 'comment':
+                postData.comments.push(newComment);
+                postData.commentNum++;
+                break;
+
+            default:
+                break;
+        }
+        //更新缓存数据库
+        allPostData[itemData.index] = postData;
+        this.execStorageSync(allPostData);
+        return postData;
+    }
+
+
+    //获取评论数据
+    getCommentData() {
+
+        var itemData = this.getPostItemById().data;
+
+        //按时间降序排列评论
+        itemData.comments.sort(this.compareWithTime);
+        var len = itemData.comments.length,
+            comment;
+        for (var i = 0; i < len; i++) {
+            //将comment中的时间戳转换成可阅读格式
+            comment = itemData.comments[i];
+            comment.create_time = util.getDiffTime(comment.create_time, true);
+        }
+        return itemData.comments;
+    }
+
+
+    //比较时间的方法
+    compareWithTime(time1, time2) {
+        var flag = parseFloat(time1.create_time) - parseFloat(time2.create_time);
+        if (flag < 0) {
+            return 1;
+        } else if (flag > 0) {
+            return -1;
         } else {
-          //如果当前状态是已收藏
-          postData.collectionNum--;
-          postData.collectionStatus = false;
+            return 0;
         }
-        break;
-      case 'up':
-        //点赞操作
-        if (!postData.upStatus) {
-          postData.upNum++;
-          postData.upStatus = true;
-        } else {
-          postData.upNum--;
-          postData.upStatus = false;
-        }
-
-      default:
-        break;
     }
-    //更新缓存数据库
-    allPostData[itemData.index] = postData;
-    this.execStorageSync(allPostData);
-    return postData;
-  }
-
-
-  //获取评论数据
-  getCommentData() {
-
-    var itemData = this.getPostItemById().data;
-
-    //按时间降序排列评论
-    itemData.comments.sort(this.compareWithTime);
-    var len = itemData.comments.length,
-      comment;
-    for (var i = 0; i < len; i++) {
-      //将comment中的时间戳转换成可阅读格式
-      comment = itemData.comments[i];
-      comment.create_time = util.getDiffTime(comment.create_time, true);
+    //发表评论
+    newComment(newComment) {
+        this.updatePostData('comment', newComment);
     }
-    return itemData.comments;
-  }
-
-
-  //比较时间的方法
-  compareWithTime(time1, time2) {
-    var flag = parseFloat(time1.create_time) - parseFloat(time2.create_time);
-    if (flag < 0) {
-      return 1;
-    } else if (flag > 0) {
-      return -1;
-    } else {
-      return 0;
-    }
-  }
 };
 
-export { DBPost }
+export {
+    DBPost
+}
 
 
 
--- pages/post/post-comment/post-comment.js
+++ pages/post/post-comment/post-comment.js
@@ -9,7 +9,11 @@ Page({
      */
     data: {
         //初始化变量值
-        useKeyboardFlag:false,
+        useKeyboardFlag: true,
+        //控制input组件的初始值
+        keyboardInputValue:'',
+        //控制是否显示图片选择面板
+        sendMoreMsgFlag: false,
     },
 
     /**
@@ -38,14 +42,14 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function() {
-		console.log("预览结束触发 onShow");
+        console.log("预览结束触发 onShow");
     },
 
     /**
      * 生命周期函数--监听页面隐藏
      */
     onHide: function() {
-		console.log("图片点击触发 onHide")
+        console.log("图片点击触发 onHide")
     },
 
     /**
@@ -76,31 +80,98 @@ Page({
 
     },
 
-	/**
-	 * 预览图片
-	 */
-	previewImg: function(event) {
-		//获取评论序号
-		var commentIdx = event.currentTarget.dataset.commentIdx,
-			//获取图片在图片数组中的序号
-			imgIdx = event.currentTarget.dataset.imgIdx,
-			//获取评论的全部图片
-			imgs = this.data.comments[commentIdx].content.img;
-		console.log("当前预览图片的路径>>>" + imgs[imgIdx]);
-		wx.previewImage({
-			current: imgs[imgIdx],//当前显示图片的http连接
-			urls: imgs//需要预览的图片http连接列表
-		})
-	},
+    /**
+     * 预览图片
+     */
+    previewImg: function(event) {
+        //获取评论序号
+        var commentIdx = event.currentTarget.dataset.commentIdx,
+            //获取图片在图片数组中的序号
+            imgIdx = event.currentTarget.dataset.imgIdx,
+            //获取评论的全部图片
+            imgs = this.data.comments[commentIdx].content.img;
+        console.log("当前预览图片的路径>>>" + imgs[imgIdx]);
+        wx.previewImage({
+            current: imgs[imgIdx], //当前显示图片的http连接
+            urls: imgs //需要预览的图片http连接列表
+        })
+    },
 
     /**
      * 评论方式切换:语音/文字
      */
-    switchInputType: function (event) {
+    switchInputType: function(event) {
         this.setData({
-            useKeyboardFlag:!this.data.useKeyboardFlag
+            useKeyboardFlag: !this.data.useKeyboardFlag
+        })
+    },
+
+    /**
+     * 获取用户输入
+     */
+    bindCommentInput: function(event) {
+        var val = event.detail.value;
+        this.data.keyboardInputValue = val;
+        return val;
+    },
+
+    //提交用户评论
+    submitComment: function(event) {
+        var newData = {
+            username: "青石",
+            avatar: "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1582432436&di=1ea28437e9d7ee2c1f3539d86c60b7d1&src=http://thumb.1010pic.com/pic10/d/m/404470_1.jpg",
+            //评论时间
+            create_time:new Date().getTime()/ 1000,
+            //评论内容
+            content: {
+                txt: this.data.keyboardInputValue,
+                txt1: 'this.data.keyboardInputValue'
+            },
+        };
+        if (!newData.content.txt) {
+            //如果没有评论内容就不执行任何操作
+            return;
+        }
+        //保存新评论到缓存数据库中
+        this.dbPost.newComment(newData);
+        //显示操作结果
+        this.showCommitSuccessToast();
+        //重新渲染并绑定所有评论
+        this.bindCommentData();
+        //恢复初始状态
+        this.resetAllDefaultStatus();
+    },
+
+    //新增评论成功
+    showCommitSuccessToast: function(){
+        //显示操作结果
+        wx.showToast({
+            title: '评论成功',
+            duration:1000,
+            icon:'success'
+        })
+    },
+    //重新绑定评论数据
+    bindCommentData: function(){
+        var comments = this.dbPost.getCommentData();
+        //绑定评论数据
+        this.setData({
+            comments:comments
+        });
+    },
+
+    //清空输入框
+    resetAllDefaultStatus: function(){
+        this.setData({
+            keyboardInputValue:""
+        });
+    },
+
+    //显示选择照片/拍照等按钮
+    sendMoreMsg: function(){
+        this.setData({
+            sendMoreMsgFlag: !this.data.sendMoreMsgFlag
         })
     }
-
 
 })
